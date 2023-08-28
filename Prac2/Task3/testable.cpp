bfefcc213f17f07e446cbace9de0a3b6a63f7894
Tim Tam <timothymdevilliers@gmail.com>
2023-08-28 21:39:53 +0200

    Reverted to old

--- Prac2/Task3/testable.cpp
+++ Prac2/Task3/testable.cpp
@@ -1,1 +1,12 @@
-#include "testable.h"
+#ifndef TESTABLE_H
+#define TESTABLE_H
+#include <string>
+#include <iostream>
+#include "test.h"
+using namespace std;
+class Testable
+{
+    public:
+        virtual bool runTest() = 0;
+};
+#endif
